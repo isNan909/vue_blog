40343e77c9ee8bc9aa1b553be5e299ed2551cb75
Ishan Manandhar <ishan.manandhar@wolfmatrix.com>
2017-07-16 20:12:04 +0545

    simple blog with routes

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import faq from '@/components/faq'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/faq',
+      name: 'Faq',
+      component: faq
+    },
+  ]
+})
