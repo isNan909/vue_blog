134295b3522275cfc748165cc11afb51c3fe90e0
Ishan Manandhar <ishan.manandhar@wolfmatrix.com>
2017-07-16 20:38:49 +0545

    sass and bulma framework integration

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,23 +10,15 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  <style lang="sass" scoped>
+  @import '../style'
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  #app
+    font-family: 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    text-align: center
+    color: #2c3e50
+    margin-top: 60px
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
+  </style>
